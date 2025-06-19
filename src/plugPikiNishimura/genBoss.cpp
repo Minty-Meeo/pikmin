ad14684746473ba65e83c6b06dcc698c2639073d
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2025-06-19 03:44:35 -0500

    First Custom Boss: Raging Long Legs!
    December 2019 called, they want their concept back.

--- src/plugPikiNishimura/genBoss.cpp
+++ src/plugPikiNishimura/genBoss.cpp
@@ -200,6 +200,10 @@ void GenObjectBoss::updateUseList(Generator*, int count)
 		bossMgr->addUseCount(BOSS_Geyzer, 1);
 		return;
 	}
+	if (mBossID == GENBOSS_Bigfoot) {
+		bossMgr->addUseCount(BOSS_Bigfoot, 1);
+		return;
+	}
 }
 
 /*
@@ -232,6 +236,8 @@ Creature* GenObjectBoss::birth(BirthInfo& info)
 		boss = bossMgr->create(GENBOSS_Mizu, info, this);
 	} else if (mBossID == GENBOSS_Geyzer) {
 		boss = bossMgr->create(GENBOSS_Geyzer, info, this);
+	} else if (mBossID == GENBOSS_Bigfoot) {
+		boss = bossMgr->create(GENBOSS_Bigfoot, info, this);
 	}
 
 	if (!boss) {
