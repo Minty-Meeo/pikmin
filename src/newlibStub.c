9ea889d61d216dd7394f73d3e7247973197da55e
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2026-01-19 23:04:13 -0600

    newlib stub

---
+++ src/newlibStub.c
@@ -0,0 +1,49 @@
+void _exit(int)
+{
+	while (1) { }
+}
+
+void close(void)
+{
+	while (1) { }
+}
+
+void lseek(void)
+{
+	while (1) { }
+}
+
+void read(void)
+{
+	while (1) { }
+}
+
+void kill(void)
+{
+	while (1) { }
+}
+
+void getpid(void)
+{
+	while (1) { }
+}
+
+void sbrk(void)
+{
+	while (1) { }
+}
+
+void write(void)
+{
+	while (1) { }
+}
+
+void fstat(void)
+{
+	while (1) { }
+}
+
+void isatty(void)
+{
+	while (1) { }
+}
