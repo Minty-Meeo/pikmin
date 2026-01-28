fdc6ac4588dab99ea83655e1eeeb170e016fc757
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2026-01-27 22:49:27 -0600

    Merged `sincostable`
    Saves 12 KiB.

--- src/sysCommon/graphics.cpp
+++ src/sysCommon/graphics.cpp
@@ -809,9 +809,8 @@ Graphics::Graphics()
 
 	mRenderMode = 0;
 
-	for (int i = 0; i < 0x1000; i++) {
-		sintable[i] = NMathF::sin(TAU * (i / 4096.0f));
-		costable[i] = NMathF::cos(TAU * (i / 4096.0f));
+	for (int i = 0; i < ARRAY_SIZE(sincostable); i++) {
+		sincostable[i] = NMathF::sin(i * (TAU / (ARRAY_SIZE(sincostable) * 4 / 5)));
 	}
 
 	mActiveTexture[0] = nullptr;
