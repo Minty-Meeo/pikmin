3194164532711d575dcdaa430b2327ca20b9c284
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2026-02-06 19:22:58 -0600

    Initialize RNG

--- src/sysBootup.cpp
+++ src/sysBootup.cpp
@@ -1,12 +1,15 @@
 #include "App.h"
+#include "Dolphin/os.h"
 #include "sysNew.h"
 #include "system.h"
+#include <stdlib.h>
 
 /**
  * @todo: Documentation
  */
 TERNARY_BUILD_MATCHING(void, int) main(int argc, char* argv[])
 {
+	srand(OSGetTick());
 	gsys->Initialise();
 	nodeMgr = new NodeMgr();
 	gsys->run(new PlugPikiApp());
